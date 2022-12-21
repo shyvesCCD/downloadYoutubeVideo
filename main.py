0f8269e6a4bdd5cbdd1943f7dd5f978d42f530fb
shyvesCCD <pedroochavess97@gmail.com>
2022-12-21 14:34:01 -0300

    bem simples

---
+++ main.py
@@ -0,0 +1,12 @@
+# script rápido pra baixar videos do youtube.
+from pytube import YouTube
+
+link = input('Qual o link para o video?')
+
+yt = YouTube(link)
+print(yt.title)
+yt = yt.streams.get_highest_resolution()
+try:
+    yt.download()
+except: 
+    print('Alguma coisa deu errada no download... Checa a url ou ')
